86d762bc43f2a934e4b042963d905ea62ad9374d
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-12-01 20:40:15 +0000

    Add auth routes

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const app = express();
 
 const PORT = 3000;
 
+const authRoute = require("./routes/auth");
+
+// Middleware
+app.use(express.json());
+
+// Route middleware
+app.use("/api/user", authRoute);
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
